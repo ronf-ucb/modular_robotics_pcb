a477154c5c3df948b6d32867853e0095619989fd
Ron Fearing <ronf@eecs.berkeley.edu>
2018-06-08 17:38:22 -0700

    DAC ramp working, A/D interrupt not triggering

--- micro_mirror/MicroMirror_k64f/source/MicroMirror_k64f.c
+++ micro_mirror/MicroMirror_k64f/source/MicroMirror_k64f.c
@@ -130,6 +130,8 @@ volatile bool timer_lockout_period = false;
  * Prototypes
  ******************************************************************************/
 void start_timer(void);
+void init_peripherals(void);
+
 /* Application API */
 extern void write_task_1(void *pvParameters);
 extern void write_task_2(void *pvParameters);
@@ -197,61 +199,6 @@ void PORTD_IRQHandler(void)
 #endif
 }
 
-static void DAC_ADC_Init(void)
-{
-    adc16_config_t adc16ConfigStruct;
-    dac_config_t dacConfigStruct;
-
-    /* Configure the DAC. */
-    /*
-     * dacConfigStruct.referenceVoltageSource = kDAC_ReferenceVoltageSourceVref2;
-     * dacConfigStruct.enableLowPowerMode = false;
-     */
-    DAC_GetDefaultConfig(&dacConfigStruct);
-    DAC_Init(DEMO_DAC_BASEADDR, &dacConfigStruct);
-    DAC_Enable(DEMO_DAC_BASEADDR, true); /* Enable output. */
-
-    /* Configure the ADC16. */
-    /*
-     * adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceVref;
-     * adc16ConfigStruct.clockSource = kADC16_ClockSourceAsynchronousClock;
-     * adc16ConfigStruct.enableAsynchronousClock = true;
-     * adc16ConfigStruct.clockDivider = kADC16_ClockDivider8;
-     * adc16ConfigStruct.resolution = kADC16_ResolutionSE12Bit;
-     * adc16ConfigStruct.longSampleMode = kADC16_LongSampleDisabled;
-     * adc16ConfigStruct.enableHighSpeed = false;
-     * adc16ConfigStruct.enableLowPower = false;
-     * adc16ConfigStruct.enableContinuousConversion = false;
-     */
-    ADC16_GetDefaultConfig(&adc16ConfigStruct);
-#if defined(BOARD_ADC_USE_ALT_VREF)
-    adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceValt;
-#endif
-    ADC16_Init(DEMO_ADC16_BASEADDR, &adc16ConfigStruct);
-
-    /* Make sure the software trigger is used. */
-    ADC16_EnableHardwareTrigger(DEMO_ADC16_BASEADDR, false);
-#if defined(FSL_FEATURE_ADC16_HAS_CALIBRATION) && FSL_FEATURE_ADC16_HAS_CALIBRATION
-    if (kStatus_Success == ADC16_DoAutoCalibration(DEMO_ADC16_BASEADDR))
-    {
-        PRINTF("\r\nADC16_DoAutoCalibration() Done.");
-    }
-    else
-    {
-        PRINTF("ADC16_DoAutoCalibration() Failed.\r\n");
-    }
-#endif /* FSL_FEATURE_ADC16_HAS_CALIBRATION */
-
-    /* Prepare ADC channel setting */
-    g_adc16ChannelConfigStruct.channelNumber = DEMO_ADC16_USER_CHANNEL;
-    g_adc16ChannelConfigStruct.enableInterruptOnConversionCompleted = true;
-
-#if defined(FSL_FEATURE_ADC16_HAS_DIFF_MODE) && FSL_FEATURE_ADC16_HAS_DIFF_MODE
-    g_adc16ChannelConfigStruct.enableDifferentialConversion = false;
-#endif /* FSL_FEATURE_ADC16_HAS_DIFF_MODE */
-}
-
-
 
 void start_timer(void)
 { char log[MAX_LOG_LENGTH + 1];
@@ -307,6 +254,9 @@ int main(void)
     PRINTF("\n\r MicroMirror Driver June 2018 v0.0\n\r");
     PRINTF("Using SW3 PTA4 or PTD1 (J2-12)for trigger, and FTM0 Ch3 (J2-4) for LED drive\n\r");
 
+
+    /* initialize peripherals, such as A/D and D/A */
+    init_peripherals();
     /* start periodic interrupt timer- should be in its own file */
       PIT_GetDefaultConfig(&pitConfig);
      	    /* Init pit module */
--- micro_mirror/MicroMirror_k64f/source/volt_control.c
+++ micro_mirror/MicroMirror_k64f/source/volt_control.c
@@ -6,14 +6,43 @@
  */
 #include "board.h"
 #include "fsl_dac.h"
+#include "fsl_adc16.h"
+#include "fsl_debug_console.h"  // PRINTF is in here
 
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
 #define DEMO_DAC_BASEADDR DAC0
+#define DEMO_ADC16_BASEADDR ADC0
+#define DEMO_ADC16_CHANNEL_GROUP 0U
+#define DEMO_ADC16_USER_CHANNEL 12U /* PTB2, ADC0_SE12 */
+#define DEMO_ADC16_IRQn ADC0_IRQn
+#define DEMO_ADC16_IRQ_HANDLER_FUNC ADC0_IRQHandler
+#define VREF_BRD 3.300
+#define SE_12BIT 4096.0
 
+/* variables */
 extern volatile uint32_t systime; //systime updated very 100 us = 4 days ==> NEED OVERFLOW protection
+volatile bool g_Adc16ConversionDoneFlag = false;
+volatile uint32_t g_Adc16ConversionValue = 0;
+extern adc16_channel_config_t g_adc16ChannelConfigStruct;
 
+
+float voltRead;
+
+/* interrupt routine */
+
+void DEMO_ADC16_IRQ_HANDLER_FUNC(void)
+{
+    g_Adc16ConversionDoneFlag = true;
+    /* Read conversion result to clear the conversion completed flag. */
+    g_Adc16ConversionValue = ADC16_GetChannelConversionValue(DEMO_ADC16_BASEADDR, DEMO_ADC16_CHANNEL_GROUP);
+    /* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F Store immediate overlapping
+      exception return operation might vector to incorrect interrupt */
+#if defined __CORTEX_M && (__CORTEX_M == 4U)
+    __DSB();
+#endif
+}
 
 /* do control here in main PIT loop.
  * 1) read voltage level
@@ -23,5 +52,19 @@ void volt_control()
 {
 	LED_GREEN_TOGGLE();
 
-	DAC_SetBufferValue(DEMO_DAC_BASEADDR, 0U, (unsigned int) (systime & 0xfff));  // 12 bit DAC
+	DAC_SetBufferValue(DEMO_DAC_BASEADDR, 0U, (unsigned int) ((systime & 0xffff)>>4));  // 12 bit DAC
+
+	/*if ( (unsigned int)(systime & 0xffff) == 0x0000)
+	{
+		g_Adc16ConversionDoneFlag = false;
+		ADC16_SetChannelConfig(DEMO_ADC16_BASEADDR, DEMO_ADC16_CHANNEL_GROUP, &g_adc16ChannelConfigStruct);
+
+		while (!g_Adc16ConversionDoneFlag) {}  // wait for A/D ready- maybe add timeout?
+
+		PRINTF("\r\n\r\nADC Value: %d\r\n", g_Adc16ConversionValue);
+		 Convert ADC value to a voltage based on 3.3V VREFH on board
+		voltRead = (float)(g_Adc16ConversionValue * (VREF_BRD / SE_12BIT));
+		PRINTF("\r\nADC Voltage: %0.3f\r\n", voltRead);
+
+	}*/
 }
